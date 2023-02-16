41f8e6274fa586fc9e2297969a30286c333e2554
plai201 <nguyenthanhtungksa@gmail.com>
2023-02-16 16:40:16 +0700

    cap nhap them web

--- MyApp/src/main/java/module-info.java
+++ MyApp/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.myapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens com.mycompany.myapp to javafx.fxml;
     exports com.mycompany.myapp;
